766732c5aa41a259bec5c272a096d2e55b93022a
Alexander Scherer <alexscherer@outlook.com>
2020-03-03 14:24:49 +0100

    Add ejs and express

--- calculator.js
+++ calculator.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const PORT = 3000;
+app.use(bodyParser());
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.listen(PORT, function() {
   console.log('Server started on PORT ' + PORT);
+  console.log('Nodejs version is ' + process.version);
+  console.log('Press Ctrl+C to quit.');
 });
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.render('index');
 });
